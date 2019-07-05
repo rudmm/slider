136639939744043749101e189e899678a49ce78d
Mykhailo Rud <rudmm1996@gmail.com>
2019-07-05 17:51:34 +0300

    exit

--- script.js
+++ script.js
@@ -29,10 +29,31 @@ btnRt.click(function(){
 		}else if(imgClass==='slideRight'){
 			images.eq(i).removeClass('slideRight');
 			images.eq(i).addClass('slide3');
+
 		}
 	}
 
 });
 btnLf.click(function(){
+		for(let i=0;i<imagesLength;i++){
+		let imgClass = images.eq(i).attr('class');
+		if(imgClass==='slideLeft'){
+			images.eq(i).removeClass('slideLeft');
+			images.eq(i).addClass('slide1');
+		}else if(imgClass==='slide1'){
+			images.eq(i).removeClass('slide1');
+			images.eq(i).addClass('slide2');
+		}else if(imgClass==='slide2'){
+			images.eq(i).removeClass('slide2');
+			images.eq(i).addClass('slide3');
+		}else if(imgClass==='slide3'){
+			images.eq(i).removeClass('slide3');
+			images.eq(i).addClass('slide4');
+		}else if(imgClass==='slideRight'){
+			images.eq(i).removeClass('slideRight');
+			images.eq(i).addClass('slideLeft');
+			
+		}
+	}
 
 });

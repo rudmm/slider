1f541d9c4ab6fcd16df8895bf882aae2b4e116df
Mykhailo Rud <rudmm@ukr.net>
2019-07-08 15:25:06 +0300

    full

--- script.js
+++ script.js
@@ -30,8 +30,25 @@ btnRt.click(function(){
 
 });
 btnLf.click(function(){
+	let slider = $('.slider img');
+	let sl = $('.slider img.slideLeft');
+	let sl1 = $('.slider img.slide1');
+	let sl2 = $('.slider img.slide2');
+	let sl3 = $('.slider img.slide3');
+	sl.removeClass('slideLeft');
+	sl.addClass('slide1');
+	sl1.removeClass('slide1');
+	sl1.addClass('slide2');
+	sl2.removeClass('slide2');
+	sl2.addClass('slide3');
+	sl3.removeClass('slide3');	
+	if(sl.prev().length>0){
+		sl.prev().addClass('slideLeft');
+	}else{
+		slider.eq(slider.length-1).addClass('slideLeft');
+	}
+
 	    let button = $(this);
-		
 	button.attr('disabled', true);
 	setTimeout(function(){
 		button.attr('disabled', false);
@@ -39,63 +56,3 @@ btnLf.click(function(){
 
 });
 
-////test
-let testLf = $('.testLf');
-let testRt = $('.testRt');
-testLf.click(function(){
-	let button = $(this);
-		for(let i=0;i<imagesLength;i++){
-		let imgClass = images.eq(i).attr('class');
-		if(imgClass==='slideLeft'){
-			images.eq(i).removeClass('slideLeft');
-			images.eq(i).addClass('slide1');
-		}else if(imgClass==='slide1'){
-			images.eq(i).removeClass('slide1');
-			images.eq(i).addClass('slide2');
-		}else if(imgClass==='slide2'){
-			images.eq(i).removeClass('slide2');
-			images.eq(i).addClass('slide3');
-		}else if(imgClass==='slide3'){
-			images.eq(i).removeClass('slide3');
-			images.eq(i).addClass('slideRight');
-		}else if(imgClass==='slideRight'){
-			images.eq(i).removeClass('slideRight');
-			images.eq(i).addClass('slideLeft');
-			
-		}
-	}
-	button.attr('disabled', true);
-	setTimeout(function(){
-		button.attr('disabled', false);
-	},700);
-});
-testRt.click(function(){
-	let button = $(this);
-
-	for(let i=0;i<imagesLength;i++){
-		let imgClass = images.eq(i).attr('class');
-		if(imgClass==='slideLeft'){
-			images.eq(i).removeClass('slideLeft');
-			images.eq(i).addClass('slideRight');
-		}else if(imgClass==='slide1'){
-			images.eq(i).removeClass('slide1');
-			images.eq(i).addClass('slideLeft');
-		}else if(imgClass==='slide2'){
-			images.eq(i).removeClass('slide2');
-			images.eq(i).addClass('slide1');
-		}else if(imgClass==='slide3'){
-			images.eq(i).removeClass('slide3');
-			images.eq(i).addClass('slide2');
-		}else if(imgClass==='slideRight'){
-			images.eq(i).removeClass('slideRight');
-			images.eq(i).addClass('slide3');
-			
-		}
-	}
-	button.attr('disabled', true);
-	setTimeout(function(){
-		button.attr('disabled', false);
-	},700);
-
-
-});
